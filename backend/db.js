182c597e53145b54bf0c95db8690ad3344a33e5b
Gustavo <gustavoho2005@gmail.com>
2024-12-31 01:40:52 -0300

    criando arquivos de rotas próprias

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,13 @@
 const express = require("express");
+const rotaLivros = require("./rotas/rotaLivros")
 
 const app = express();
 
 const PORT = 8000;
 
+app.use("/livros", rotaLivros)
 
-app.get('/api', (req, res) => {
 
-        res.send("Hello World")
-
-});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
